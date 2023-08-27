6292bf143cbb2f26660481107eb65afe37637589
Davut Durmaz <124710876+davutD@users.noreply.github.com>
2023-08-27 03:23:06 +0300

    some styling for editor page

--- frontend/components/EditorQuill.vue
+++ frontend/components/EditorQuill.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="w-2/3 m-auto mt-10">
-    <QuillEditor theme="snow" toolbar="full" contentType="html" v-model:content="content"/>
-    <button @click="displayContent" class="border flex m-auto rounded-lg mt-5">Click</button>
+  <div class="w-2/3 m-auto mt-10 flex flex-col border p-2">
+    <input type="text" class="title text-2xl" required placeholder="Enter a title for article...">
+    <input type="text" required placeholder="Briefly mention what it is about...">
+    <div class="flex flex-row my-auto justify-between mb-8">
+      <label class="mx-5 my-auto">Tags:</label>
+      <input type="text" required placeholder="#1">
+      <input type="text" placeholder="#2">
+      <input type="text" placeholder="#3">      
+    </div>
+    <QuillEditor theme="snow" toolbar="full" placeholder="Compose a story..." contentType="html" v-model:content="content"/>
+    <button class="border flex m-auto rounded-lg mt-5 pt-1">Publish</button>
     <div v-html="content"></div>
   </div>
 </template>
@@ -11,23 +19,29 @@ import { QuillEditor,Quill } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
 import { useUserStore } from '../store/UserStore'
 
-const userStore=useUserStore()
 const content=ref('')
-const router=useRouter()
-
-onMounted(()=>{
-  if(userStore.token==null && userStore.user==null)
-  {
-    router.push('/login')
-  }
-})
-
-const displayContent=()=>{
-    console.log(content)
-}
 
 </script>
 
 <style lang="css">
-
+.ql-editor{
+  height: 10rem;
+}
+input::placeholder{
+  font-style: italic;
+}
+input{
+  padding: 0 1rem;
+  height: 2.5rem;
+  margin-bottom: 0.5rem;
+  border-bottom: 1px solid #e0e0e0;
+}
+input[type="text"]:focus,
+textarea:focus,
+input[type="email"]:focus,
+input[type="password"]:focus {
+  border: 1px solid transparent;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.2); /* Example of a subtle box-shadow */
+  outline: none;
+}
 </style>
--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,5 +1,18 @@
 <template>
-    <v-toolbar color="blue" :elevation="8">
+    <div class="header border w-full h-16 flex">
+        <div class="border w-11/12 h-12 m-auto flex flex-row justify-between">
+            <div class="my-auto mx-2 flex flex-row">
+                <button @click="homePage" class="home-page my-auto mx-2 px-10 text-3xl">HOME</button>
+                <button @click="editorPage" v-if="userStore.isUserLoggedIn" class="my-auto mx-2">Editor</button>
+            </div>
+            <div class="flex flex-row list-none my-auto">
+                <button @click="login" v-if="!userStore.isUserLoggedIn" class="mx-2">Login</button>
+                <button @click="register" v-if="!userStore.isUserLoggedIn" class="mx-2">Register</button>
+                <button @click="logout" v-if="userStore.isUserLoggedIn" class="mx-2">Log Out</button>
+            </div>
+        </div>
+    </div>
+    <!-- <v-toolbar color="blue" :elevation="8">
             <v-toolbar-items>
                 <NuxtLink class="h-full" to="/"><v-btn>Home</v-btn></NuxtLink>
                 <NuxtLink class="h-full" to="/editor"><v-btn>Editor</v-btn></NuxtLink>
@@ -7,23 +20,43 @@
                 <NuxtLink v-if="!userStore.isUserLoggedIn" class="h-full" to="/register"><v-btn>Register</v-btn></NuxtLink>
                 <NuxtLink v-if="userStore.isUserLoggedIn" @click="logout" class="h-full" to="/"><v-btn>Log Out</v-btn></NuxtLink>
             </v-toolbar-items>
-    </v-toolbar>
+    </v-toolbar> -->
 </template>
 
 <script setup>
 import { useUserStore } from '../store/UserStore';
 
 const userStore=useUserStore()
+const router=useRouter()
 
+const homePage=()=>{
+    router.push('/')
+}
+const editorPage=()=>{
+    router.push('/editor')
+}
+const login=()=>{
+    router.push('/login')
+
+}
+const register=()=>{
+    router.push('/register')
+
+}
 const logout=()=>{
     userStore.setToken(null)
     userStore.setUser(null)
+    router.push('/')
 }
 
 </script>
 
 <style lang="css">
+.header{
+    background-color: #ffc017;
+}
 button{
-
+    height: 2rem;
+    padding: 0 1rem;
 }
 </style>
